ffcd86ff7b8a15f2fa2a517a07ee4923929dec00
Paul Gregoire <mondain@gmail.com>
2021-08-09 08:44:49 -0700

    Formatting most likely

--- src/main/java/com/red5pro/server/plugin/simpleauth/datasource/impl/roundtrip/RoundTripAuthValidator.java
+++ src/main/java/com/red5pro/server/plugin/simpleauth/datasource/impl/roundtrip/RoundTripAuthValidator.java
@@ -249,7 +249,8 @@ public class RoundTripAuthValidator implements IAuthenticationValidator, IApplic
 	/**
 	 * Special method to help validate cluster restreamer using the cluster password
 	 *
-	 * @param password The provided password to validate
+	 * @param password
+	 *            The provided password to validate
 	 * @return Boolean true if validation is successful, otherwise false
 	 */
 	private boolean validateClusterReStreamer(String password) {
@@ -279,9 +280,12 @@ public class RoundTripAuthValidator implements IAuthenticationValidator, IApplic
 	/**
 	 * Authenticates a publisher connection, via remote server
 	 *
-	 * @param conn   The IConnection object representing the connection
-	 * @param scope  The scope where client is attempting to publishing
-	 * @param stream The stream name that the client is trying to publish
+	 * @param conn
+	 *            The IConnection object representing the connection
+	 * @param scope
+	 *            The scope where client is attempting to publishing
+	 * @param stream
+	 *            The stream name that the client is trying to publish
 	 *
 	 * @return Boolean true if validation is successful, otherwise false
 	 */
@@ -321,9 +325,12 @@ public class RoundTripAuthValidator implements IAuthenticationValidator, IApplic
 	/**
 	 * Authenticates a subscriber connection, via remote server
 	 *
-	 * @param conn   The IConnection object representing the connection
-	 * @param scope  The scope where client is attempting to subscribe
-	 * @param stream The stream name that the client is trying to subscribe to
+	 * @param conn
+	 *            The IConnection object representing the connection
+	 * @param scope
+	 *            The scope where client is attempting to subscribe
+	 * @param stream
+	 *            The stream name that the client is trying to subscribe to
 	 *
 	 * @return Boolean true if validation is successful, otherwise false
 	 */
@@ -363,11 +370,16 @@ public class RoundTripAuthValidator implements IAuthenticationValidator, IApplic
 	/**
 	 * Method to authentication/validate client via remote server over http/https
 	 *
-	 * @param type     The client type to validate - `publisher or subscriber`
-	 * @param username The `username` parameter provided by the client
-	 * @param password The `password` parameter provided by the client
-	 * @param token    The `token` parameter provided by the client
-	 * @param stream   The `stream name` for which validation is required
+	 * @param type
+	 *            The client type to validate - `publisher or subscriber`
+	 * @param username
+	 *            The `username` parameter provided by the client
+	 * @param password
+	 *            The `password` parameter provided by the client
+	 * @param token
+	 *            The `token` parameter provided by the client
+	 * @param stream
+	 *            The `stream name` for which validation is required
 	 *
 	 * @return JsonObject JSON payload response from the remote server
 	 */
@@ -426,10 +438,14 @@ public class RoundTripAuthValidator implements IAuthenticationValidator, IApplic
 	/**
 	 * Method to invalidate client via remote server over http/https
 	 *
-	 * @param username The `username` parameter provided by the client
-	 * @param password The `password` parameter provided by the client
-	 * @param token    The `token` parameter provided by the client
-	 * @param stream   The `stream name` for which validation is required
+	 * @param username
+	 *            The `username` parameter provided by the client
+	 * @param password
+	 *            The `password` parameter provided by the client
+	 * @param token
+	 *            The `token` parameter provided by the client
+	 * @param stream
+	 *            The `stream name` for which validation is required
 	 *
 	 * @return JsonObject JSON payload response from the remote server
 	 */
