91104054a34f89ce2c495a9a350566e747453715
david179 <31712615+david179@users.noreply.github.com>
2021-09-07 12:57:47 -0400

    authenticate websocket connections in mock server

--- nodejs-mock-service/index.js
+++ nodejs-mock-service/index.js
@@ -44,7 +44,7 @@ var host = "localhost";
 *   PORT
 *
 */
-var port  = 3000;
+var port = 3000;
 /*
 *   Optional url resource for authenticated client
 *
@@ -58,7 +58,7 @@ var optionalURLResource = ""; // optional
 
 
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 
@@ -68,54 +68,54 @@ var useToken = false;
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 // get POST to log In
-app.post('/logIn', function(request, response){
+app.post('/logIn', function (request, response) {
 
-    console.log(request.body.username);
-    console.log(request.body.password);
-	
-	response.send(JSON.stringify({"username":"streamUsername","password":"streamPassword","streamID":"streamID"}));
-	
+	console.log(request.body.username);
+	console.log(request.body.password);
+
+	response.send(JSON.stringify({ "username": "streamUsername", "password": "streamPassword", "streamID": "streamID" }));
+
 });
 
 
 // get POST to validate the credentials for a stream
-app.post('/validateCredentials', function(request, response){
+app.post('/validateCredentials', function (request, response) {
 
-    console.log('\n\nvalidate credentials called');
-	console.log('type: '+request.body.type);
-    console.log('username: '+request.body.username);
-    console.log('password: '+request.body.password);
-	console.log('streamID: '+request.body.streamID);
-    if (useToken)
-        console.log('token: '+request.body.token);
+	console.log('\n\nvalidate credentials called');
+	console.log('type: ' + request.body.type);
+	console.log('username: ' + request.body.username);
+	console.log('password: ' + request.body.password);
+	console.log('streamID: ' + request.body.streamID);
+	if (useToken)
+		console.log('token: ' + request.body.token);
 
 	var type = request.body.type;
-	
-	if (type == "publisher")
-		response.send(JSON.stringify({"result":true,"url":optionalURLResource}));
+
+	if (type == "publisher" || type == "websocket")
+		response.send(JSON.stringify({ "result": true, "url": optionalURLResource }));
 	else if (type == "subscriber")
-		response.send(JSON.stringify({"result":true}));
-	else
-	{
+		response.send(JSON.stringify({ "result": true }));
+	else {
 		console.log('invalid type supplied');
-		response.send(JSON.stringify({"result":false}));
+		response.send(JSON.stringify({ "result": false }));
 	}
 });
 
 // get POST to invalidate the credentials for a stream
-app.post('/invalidateCredentials', function(request, response){
+app.post('/invalidateCredentials', function (request, response) {
 
-    console.log('\n\ninvalidate credentials called');
-    console.log('username: '+request.body.username);
-    console.log('password: '+request.body.password);
-	console.log('streamID: '+request.body.streamID);
-    if (useToken)
-        console.log('token: '+request.body.token);
-	
-	response.send(JSON.stringify({"result":true}));
-	
+	console.log('\n\ninvalidate credentials called');
+	console.log('type: ' + request.body.type);
+	console.log('username: ' + request.body.username);
+	console.log('password: ' + request.body.password);
+	console.log('streamID: ' + request.body.streamID);
+	if (useToken)
+		console.log('token: ' + request.body.token);
+
+	response.send(JSON.stringify({ "result": true }));
+
 });
 
 
-app.listen(port,host);
+app.listen(port, host);
 
